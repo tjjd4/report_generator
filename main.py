bd83d6e85fcd5d62f84f0776947f79dbe86aeba5
billy billy <seanhung2000@gmail.com>
2023-08-22 18:45:04 +0800

    read csv

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import csv
+
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 
+GROUP1_ROW = 2
+GROUP2_ROW = 3
+GROUP3_ROW = 4
+GROUP4_ROW = 5
+GROUP5_ROW = 6
+GROUP6_ROW = 7
+GROUP7_ROW = 8
+GROUP8_ROW = 9
+
+FINAL_RANK_COL = 14
+
 w, h = A4
+with open("./score_sheet - scoresheet1.csv", 'r', encoding="utf8", newline='') as file:
+  csvreader = csv.reader(file)
+  scoresheet = list(csvreader)
+  
 
 pdfmetrics.registerFont(UnicodeCIDFont('STSong-Light'))
 
